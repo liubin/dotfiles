06d1ef3060d0b0a1aa324459f154d3d0cd41f3d8
bin liu <liubin0329@gmail.com>
2014-08-01 16:32:02 +0800

    add git prompt color settings

---
+++ init.sh
@@ -0,0 +1,5 @@
+
+# for git color
+source ~/.dotfiles/scripts/git-prompt.sh 
+export PS1="\[\e[0;32m\]\t\[\e[0;31m\]-\[\e[1;34m\]\u\[\e[1;33m\]\[\e[1;33m\]\w\[\033[m\]\[\e[1;36m\]\$(__git_ps1)\[\e[0;30m\]\ -> "
+
